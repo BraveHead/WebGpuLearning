794b49d679226dd5c7d9e9ff4c15bacf3f797df8
sanye <sanye@startdt.com>
2023-12-13 16:02:12 +0800

    feat: 绘制正方形

--- src/gameOfLife.ts
+++ src/gameOfLife.ts
@@ -1,3 +1,5 @@
+import GameOfVert from './shaders/gameOfLife.vert.wgsl?raw'
+
 async function init() {
     // 第一步 获取device
     const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
@@ -20,10 +22,76 @@ async function init() {
         device,
         format: canvasFormat
     })
-
     if(!context) {
         return
     }
+    const vertices = new Float32Array([
+        //   X,    Y,
+        -0.8, -0.8, // Triangle 1 (Blue)
+        0.8, -0.8,
+        0.8,  0.8,
+
+        -0.8, -0.8, // Triangle 2 (Red)
+        0.8,  0.8,
+        -0.8,  0.8,
+    ])
+
+    // 创建订点缓冲区
+    const vertexBuffer = device.createBuffer({
+        label: 'Cell vertices',
+        size: vertices.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+
+    // 写入buffer到缓冲区
+    device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+    // 定义顶点布局
+    const vertexBufferLayout : GPUVertexBufferLayout = {
+        arrayStride: 2 * 4,
+        attributes: [
+            {
+                format: 'float32x2',
+                offset: 0,
+                shaderLocation: 0
+            }
+        ]
+    };
+
+    // 定义定点着色器
+    const cellShaderModule = device.createShaderModule({
+        label: 'Cell shader',
+        code: `
+            @vertex
+            fn vertextMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+                return vec4f(pos,0,1); // (X, Y, Z, W)
+            }
+            @fragment
+                fn fragmentMain() -> @location(0) vec4f {
+                return vec4f(1, 0, 0, 1);
+            }
+        `
+    })
+
+    // 创建渲染流水线
+    const cellPipeline = device.createRenderPipeline({
+        label: 'Cell pipeline',
+        layout: 'auto',
+        vertex: {
+            module: cellShaderModule,
+            entryPoint: 'vertextMain',
+            buffers: [vertexBufferLayout]
+        },
+        fragment: {
+            module: cellShaderModule,
+            entryPoint: 'fragmentMain',
+            targets: [
+                {
+                    format: canvasFormat
+                }
+            ]
+        }
+    })
 
     // 生成清除画布指令
     const encoder = device.createCommandEncoder();
@@ -42,6 +110,9 @@ async function init() {
             }
         ]
     });
+    pass.setPipeline(cellPipeline);
+    pass.setVertexBuffer(0, vertexBuffer);
+    pass.draw(vertices.length / 2); // 6 vertices
     pass.end();
 
     // 获取指令缓冲区
